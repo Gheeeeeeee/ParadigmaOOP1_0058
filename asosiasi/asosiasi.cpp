6e04fa40698610a13b569e70cd79ba1c902abb74
gheeeeeeee <gheoputra55@gmail.com>
2024-05-20 19:10:35 +0700

    memasukkan variabel kedalam main fungsi

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -34,11 +34,48 @@ public:
     void cetakpasien();
 };
 
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { //auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diterapkan
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void dokter::tambahpasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+    pPasien->tambahDokter(this);
+}
+void dokter::cetakpasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
 
 
+    int main(){
+        dokter* varDokter1 = new dokter("dr.Budi");
+        dokter* varDokter2 = new dokter("dr.Tono");
+        pasien* varPasien1 = new pasien("Andi");
+        pasien* varPasien2 = new pasien("Lia");
 
-int main()
-{
-    std::cout << "Hello World!\n";
+        varDokter1->cetakpasien();
+        varDokter2->cetakpasien();
+        varPasien1->cetakDokter();
+        varPasien2->cetakDokter();
+
+        delete varPasien1;
+        delete varPasien2;
+        delete varDokter1;
+        delete varDokter2;
+
+        return 0;
+
+
 }
 
