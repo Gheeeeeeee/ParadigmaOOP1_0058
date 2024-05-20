2155fe641072d73aa0f17eaba866080af714bfb4
gheeeeeeee <gheoputra55@gmail.com>
2024-05-20 18:46:49 +0700

    asosiasi

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -3,6 +3,22 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+class pasien {// asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) {
+        cout << "Pasien \"" << nama << "\"ada\n";
+    }
+    ~pasien() {
+        cout << "pasien \"" <<nama <<"\"tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+
+
 int main()
 {
     std::cout << "Hello World!\n";
