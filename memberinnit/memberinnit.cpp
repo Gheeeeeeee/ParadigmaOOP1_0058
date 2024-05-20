d948a91ba332358c8e7d59b8902d14e8e9b848b1
gheeeeeeee <gheoputra55@gmail.com>
2024-05-20 16:04:08 +0700

    menambahkan header

--- memberinnit/memberinnit.cpp
+++ memberinnit/memberinnit.cpp
@@ -1,10 +1,4 @@
-// memberinnit.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+using namespace std;
 
