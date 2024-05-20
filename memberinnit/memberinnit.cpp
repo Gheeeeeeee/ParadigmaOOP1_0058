c8f19fe151b85651815a4757d7c5231c3e239b51
gheeeeeeee <gheoputra55@gmail.com>
2024-05-20 16:05:53 +0700

    member innit

--- memberinnit/memberinnit.cpp
+++ memberinnit/memberinnit.cpp
@@ -12,7 +12,7 @@ private:
 public:
     mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
     {
-        //member initialization list
+        //member initialization list 
         //defenisi
     }
 
