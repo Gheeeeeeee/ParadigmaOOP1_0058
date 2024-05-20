4f14108fa749b7d0ec966f0c83870a5219aa7c9b
gheeeeeeee <gheoputra55@gmail.com>
2024-05-20 16:05:40 +0700

    membuat akses modif publik

--- memberinnit/memberinnit.cpp
+++ memberinnit/memberinnit.cpp
@@ -9,4 +9,22 @@ private:
     string nama;
     float nilai;
 
+public:
+    mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+    {
+        //member initialization list
+        //defenisi
+    }
+
+    ~mahasiswa() {
+        cout << "Id     = " << id << endl;
+        cout << "Nama   = " << nama << endl;
+        cout << "Nilai  = " << nilai << endl;
+    }
+};
+
+
+int main()
+{
+    
 };
